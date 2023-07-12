8bf53c9fa1a3ab2d1f33cf939ff6cfb99e8ccebc
Edward Tanguay (windows) <edward@tanguay.info>
2023-07-12 15:59:47 +0200

    reading statuses from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
+import axios from 'axios';
+
+const url = 'http://localhost:4701/techStatuses';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [techStatuses, setTechStatuses] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	useEffect(() => {
+		(async () => {
+      const response = await axios.get(url);
+      const _techStatuses = response.data;
+      setTechStatuses(_techStatuses);
+
+		})();
+	}, []);
+
+	return (
+		<div>
+			<div>There are {techStatuses.length} statuses.</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
